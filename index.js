3f4c9591066a4b7efbfb8838191ee1bb8b0ecd29
jamest11 <jameswtory@gmail.com>
2022-10-05 15:18:43 -0400

    Create home.html and associated route

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+const PORT = 8081;
+
+console.log(process.env.port)
 /*
 - Create new html file name home.html 
 - add <h1> tag with message "Welcome to ExpressJs Tutorial"
 - Return home.html page to client
 */
 router.get('/home', (req,res) => {
-  res.send('This is home router');
+    res.sendFile( __dirname + '/' + 'home.html');
 });
 
 /*
 - Return all details from user.json file to client as JSON format
 */
 router.get('/profile', (req,res) => {
-  res.send('This is profile router');
+    res.send('This is profile router');
 });
 
 /*
@@ -38,7 +41,7 @@ router.get('/profile', (req,res) => {
     }
 */
 router.get('/login', (req,res) => {
-  res.send('This is login router');
+        res.send('This is login router');
 });
 
 /*
@@ -46,11 +49,11 @@ router.get('/login', (req,res) => {
     in HTML format like <b>${username} successfully logout.<b>
 */
 router.get('/logout', (req,res) => {
-  res.send('This is logout router');
+    res.send('This is logout router');
 });
 
 app.use('/', router);
 
-app.listen(process.env.port || 8081);
+app.listen(process.env.port || PORT);
 
-console.log('Web Server is listening at port '+ (process.env.port || 8081));
+console.log('Web Server is listening at port '+ (process.env.port || PORT));
